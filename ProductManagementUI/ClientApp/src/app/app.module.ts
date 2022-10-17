6505afdd468f23b105f5e29e8de39294f7da7b20
Ian Harriott <IHarriott@geico.com>
2022-10-17 19:43:40 +0000

    Merged PR 2621673: updated aks file
    Related work items: #6774816

--- ProductManagementUI/ClientApp/src/app/app.module.ts
+++ ProductManagementUI/ClientApp/src/app/app.module.ts
@@ -7,7 +7,17 @@ import { ComponentModule } from './components/component/component.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { TokenInterceptorService } from './services/token-interceptor.service';
-
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import {
+    MsalGuard,
+    MsalInterceptor,
+    MsalModule,
+    MsalRedirectComponent
+} from '@azure/msal-angular';
+import { environment } from '../environments/environment';
+const isIE =
+    window.navigator.userAgent.indexOf('MSIE') > -1 ||
+    window.navigator.userAgent.indexOf('Trodemt') > -1;
 @NgModule({
     declarations: [AppComponent],
     imports: [
@@ -15,7 +25,32 @@ import { TokenInterceptorService } from './services/token-interceptor.service';
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
-        ComponentModule
+        ComponentModule,
+        MsalModule.forRoot(
+            new PublicClientApplication({
+                auth: {
+                    clientId: environment.azureadClientid,
+                    redirectUri: 'http://localhost:4200',
+                    authority: `https://login.microsoftonline.com/${environment.azureadTenantid}`
+                },
+                cache: {
+                    cacheLocation: 'localStorage',
+                    storeAuthStateInCookie: isIE
+                }
+            }),
+            {
+                interactionType: InteractionType.Redirect,
+                authRequest: {
+                    scopes: ['user.read']
+                }
+            },
+            {
+                interactionType: InteractionType.Redirect,
+                protectedResourceMap: new Map([
+                    ['https://graph.microsoft.com/v1.0/me', ['user.Read']]
+                ])
+            }
+        )
     ],
     schemas: [NO_ERRORS_SCHEMA],
     providers: [
@@ -25,10 +60,11 @@ import { TokenInterceptorService } from './services/token-interceptor.service';
         },
         {
             provide: HTTP_INTERCEPTORS,
-            useClass: TokenInterceptorService,
+            useClass: MsalInterceptor,
             multi: true
-        }
+        },
+        MsalGuard
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent, MsalRedirectComponent]
 })
 export class AppModule {}
