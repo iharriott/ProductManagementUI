6f2249a7a1b7cfe3604787e1f5e1c74a37a91c3a
Pothiraj Bethanasamy <PBethanasamy@geico.com>
2022-11-23 19:20:59 +0000

    Merged PR 2685009: Added Env config settings
    Added Env config settings
    
    Related work items: #7017920

---
+++ ProductManagementUI/Controllers/ConfigurationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace ProductManagementUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            return new Dictionary<string, string>
+            {
+                { "ProductMgmtAPIBaseURL", _configuration["ProductMgmtAPIBaseURL"] },
+                { "TenantId", _configuration["TenantId"] },
+                { "ClientId", _configuration["ClientId"] },                
+                { "Authority", _configuration["Authority"] },
+                { "RedirectUrl", _configuration["RedirectUrl"] },
+                { "PostLogoutUrl", _configuration["PostLogoutUrl"] }
+            };
+        }
+    }
+}
