a082106e8be6afb6c3dfbc1c111d3badfa29d6e6
Pothiraj Bethanasamy <PBethanasamy@geico.com>
2022-12-05 18:33:51 +0000

    Merged PR 2703018: Added test cases for data service
    Added test cases for data service
    
    Related work items: #7017956

--- ProductManagementUI/ClientApp/src/app/services/data.service.spec.ts
+++ ProductManagementUI/ClientApp/src/app/services/data.service.spec.ts
@@ -13,7 +13,7 @@ describe('DataService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-
+    
     it('it should test getLoggedInUser', () => {
         service.getLoggedInUser = jest.fn().mockReturnValue('Ian Harriott');
         const loggedInUser = service.getLoggedInUser();
@@ -82,5 +82,47 @@ describe('DataService', () => {
         const newList = ['mark', 'john', 'mary', 'bob'];
         const result = service.addToList(data, value);
         expect(result).toEqual(newList);
+    });
+
+    it('it should test getProductMgmtAPIBaseURL', () => {
+        service.getProductMgmtAPIBaseURL = jest.fn().mockReturnValue('Genesis');
+        const ProductMgmtAPIBaseURL = service.getProductMgmtAPIBaseURL();
+        expect('Genesis').toEqual(ProductMgmtAPIBaseURL);
+    });
+
+    it('it should test getTenantId', () => {
+        service.getTenantId = jest.fn().mockReturnValue('Genesis');
+        const TenantId = service.getTenantId();
+        expect('Genesis').toEqual(TenantId);
+    });
+
+    it('it should test getClientId', () => {
+        service.getClientId = jest.fn().mockReturnValue('Genesis');
+        const ClientId = service.getClientId();
+        expect('Genesis').toEqual(ClientId);
+    });
+
+    it('it should test getAuthority', () => {
+        service.getAuthority = jest.fn().mockReturnValue('Genesis');
+        const Authority = service.getAuthority();
+        expect('Genesis').toEqual(Authority);
+    });
+
+    it('it should test getRedirectUrl', () => {
+        service.getRedirectUrl = jest.fn().mockReturnValue('Genesis');
+        const RedirectUrl = service.getRedirectUrl();
+        expect('Genesis').toEqual(RedirectUrl);
+    });
+
+    it('it should test getPostLogoutUrl', () => {
+        service.getPostLogoutUrl = jest.fn().mockReturnValue('Genesis');
+        const PostLogoutUrl = service.getPostLogoutUrl();
+        expect('Genesis').toEqual(PostLogoutUrl);
+    });
+
+    it('it should test getApiScope', () => {
+        service.getApiScope = jest.fn().mockReturnValue('Genesis');
+        const ApiScope = service.getApiScope();
+        expect('Genesis').toEqual(ApiScope);
     });
 });
